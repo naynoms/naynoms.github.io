23b8e6ae46e28747cbf1bd431b0cf1684e74f114
Naomi O'Sullivan <osullivan.naomi@gmail.com>
2017-06-20 10:33:16 +1000

    light title

--- js/main.js
+++ js/main.js
@@ -1,62 +1,15 @@
-var app = app || {};
-// console.log(_.VERSION);
 
-// console.log('hello world');
 $(document).ready(function () {
 
-  // var colours = ['#ff0000', '#ff8000', '#ffff00', '#80ff00', '#00ff00', '#00ff80', '#00ffff', '#0080ff', '#0000ff', '#7f00ff', '#ff00ff', '#ff007f'];
-
-  // var colour = ['#8f52a5', '#ab78d5', '#c793db', '#d0a4e3', '#e8d4f0'];
-
-  // var colour = ['#6BFFCE', '#77DAC3', '#83B5B8', '#8E90AC', '#9A6BA1', '#A64696'];
-//
-//   var colours = ['#c9f0ff', '#eafffd', '#efeff0'];
-//
-// var $circle = $('.title')
-// // .css('background-color', 'rgb(255,99,177)')
-// $circle.css('background-color', colours);
-//
-// _.each(colours, function(c) {
-//   console.log(c);
-// });
-//
-// var cycle =   _.each(colours, function(c) {
-//     console.log(c);
-//   });
-//
-//   var random =
-//
-// // $circle.on('mouseover', function () {
-// //   console.log('hi');
-// //   $(this).animate({
-// //     'background-color': (_.sample(colours))
-// //   }, 500);
-//
-// // });
-//
-// $circle.mouseover( function () {
-//   // console.log('hi');
-//   $(this).css('background-color', (_.sample(colours)));
-//   // console.log($(this).css('background-color'));
-// });
-
-// $circle.hover(
-//
-//     function(){
-//         $(this).animate({'backgroundColor': '#f5f5f5'},400);
-//         console.log('hello');
-//     },
-//     function(){
-//         $(this).animate({'backgroundColor': '#000'},400);
-//         console.log('bye');
-//     }
-//
-// );
+var scrollT = $(window).scrollTop();
+console.log(scrollT);
 
 
 var navHeight = $('nav').position().top;
 
 $(window).bind('scroll', function() {
+
+
 
   if ($(window).scrollTop() >= navHeight) {
     $('nav').addClass('fixed');
@@ -65,8 +18,24 @@ $(window).bind('scroll', function() {
   else {
     $('nav').removeClass('fixed');
     $('div.projects').removeClass('compensate');
-
   }
+
+  // var scrollT = $(window).scrollTop();
+  // console.log(scrollT);
+  // if(scrollT >= 100) {
+  //   $("#nav").css({
+  //     'padding-bottom': '10%',
+  //     'background-color': 'red'
+  //
+  //   })
+  // } else {
+  //   $("#nav").css({
+  //     'padding-bottom': '30%',
+  //     'background-color': 'blue'
+  //
+  //   })
+  // }
+
 });
 
 
